68281796653855185d80e56560a1ff0cdb44d371
geek-3340 <crew.sento0864@gmail.com>
2025-08-23 23:50:53 +0900

    PC画面　contact以外完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-const $jsHero = document.getElementById('js-hero');
+const $hero = document.getElementById('js-hero');
 
 // heroセクションが表示されたときにフェードイン
-window.addEventListener('load',() => {
-    $jsHero.classList.add('fade-in');
+window.addEventListener('DOMContentLoaded', () => {
+    $hero.classList.add('fade-in');
 });
+
+if (window.matchMedia('(max-width: 767px)').matches) {
+
+    const $humButton = document.getElementById('js-humButton');
+    const $humLines = document.querySelectorAll('.hum-lines');
+    const $siteNav=document.getElementById('js-siteNav');
+
+    const humHandler = () => {
+        $humLines.forEach((line) => {
+            if (!line.classList.contains('is-active')) {
+                line.classList.add('is-active');
+                $siteNav.classList.add('is-active');
+            } else {
+                line.classList.remove('is-active');
+                $siteNav.classList.remove('is-active');
+            }
+        });
+    };
+
+    $humButton.addEventListener('click', humHandler);
+}
