00c922af06eabaecb0c3e0bdd069a344e2f12105
geek-3340 <crew.sento0864@gmail.com>
2025-08-24 00:48:57 +0900

    メニューアクティブ時のfilter追加

--- src/main.js
+++ src/main.js
@@ -6,22 +6,29 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 if (window.matchMedia('(max-width: 767px)').matches) {
-
     const $humButton = document.getElementById('js-humButton');
     const $humLines = document.querySelectorAll('.hum-lines');
-    const $siteNav=document.getElementById('js-siteNav');
+    const $siteNav = document.getElementById('js-siteNav');
+    const $filter = document.getElementById('js-bgFilter');
 
     const humHandler = () => {
         $humLines.forEach((line) => {
             if (!line.classList.contains('is-active')) {
                 line.classList.add('is-active');
-                $siteNav.classList.add('is-active');
             } else {
                 line.classList.remove('is-active');
-                $siteNav.classList.remove('is-active');
             }
         });
     };
+    const navHandler = () => {
+        if (!$siteNav.classList.contains('is-active')) {
+            $siteNav.classList.add('is-active');
+            $filter.classList.add('is-active');
+        } else {
+            $siteNav.classList.remove('is-active');
+            $filter.classList.remove('is-active');
+        }
+    };
 
-    $humButton.addEventListener('click', humHandler);
+    $humButton.addEventListener('click', (humHandler, navHandler));
 }
